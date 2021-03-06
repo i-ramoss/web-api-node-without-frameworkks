bbe29d9a64d5f7b2279cc787fc95297f664b5f9b
Ian_Ramos <ianramossantos@hotmail.com>
2021-03-05 22:55:25 -0300

    Fix url and add scripts to all requests on package.json

--- script.sh
+++ script.sh
@@ -6,5 +6,13 @@ curl localhost:3000/heroes/1
 
 echo -e "\n\n requesting with wrong body"
 curl --silent -X POST \
-    --data-binary "{'invalid': 'data'}" \
-    localhost:3000/hereos
+    --data-binary '{"invalid": "data"}' \
+    localhost:3000/heroes
+
+
+echo -e "\n\n creating black panther"
+CREATE=$(curl --silent -X POST \
+    --data-binary '{"name": "Black Panther", "age": 35, "power": "Agility"}' \
+    localhost:3000/heroes)
+
+echo $CREATE
